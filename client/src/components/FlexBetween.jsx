abd76bf1182ef6e4cfe9e6c25a1ea46105bea330
Md Manik <mdmanikofficial0@gmail.comgit config --global user.email mdmanikofficial0@gmail.comgit config --global user.email mdmanikofficial0@gmail.com>
2023-02-01 07:27:40 +0600

    complete navbar login register pages

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import {styled} from "@mui/system";
+
+const flexBetween = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between"
+})
+
+export default flexBetween;
